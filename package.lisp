f7deb9a00453a27e388f48821fe1bbf2a9cc019f
Florian MARGAINE <florian@margaine.com>
2016-02-02 23:09:11 +0100

    Add the exports.

--- package.lisp
+++ package.lisp
@@ -1,7 +1,39 @@
 (defpackage #:linit
   (:use #:cl)
   (:export :main
-           :defservice))
+
+           ;; service
+           :defservice
+           :list-services
+           :pid
+           :state
+           :name
+           :start
+           :before
+           :after
+           :before-stopped
+           :after-stopped
+           :find-service
+           :find-service-by-pid
+           :replace-service
+           :add-service
+           :load-services
+           :load-service
+           :start-services
+           :start-service
+
+           ;; graph
+           :make-dag
+           :root-elements
+           :has-cycle
+           :start-graph-services
+           :service
+           :children
+           :parents
+           :find-graph-element-by-service-name))
 
 (defpackage #:linit-user
-  (:use #:cl #:linit))
+  (:use #:cl #:linit)
+  (:export :reboot
+           :halt
+           :exec))
